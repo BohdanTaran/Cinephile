09d2c52400ff7b35ecc8cce42e1d69b3570d6daf
BohdanTaran <bohdantaran.work@gmail.com>
2024-06-22 17:00:15 +0200

    refactor filmsList

--- src/widgets/FilmsList/ui/FilmsList.tsx
+++ src/widgets/FilmsList/ui/FilmsList.tsx
@@ -1,15 +1,17 @@
 import { FilmCard } from '../../../entities/films';
 import { IFilm } from '../../../entities/films/model/types';
+import { IGenre } from '../../../shared/ui/Genres/model/types';
 
 interface Props {
   films?: IFilm[];
+  genres: IGenre[];
 }
 
-export const FilmsList = ({ films }: Props) => {
+export const FilmsList = ({ films, genres }: Props) => {
   return (
     <>
       {films?.map((film) => {
-        return <FilmCard key={film.id} film={film} />;
+        return <FilmCard key={film.id} genres={genres} film={film} />;
       })}
     </>
   );
