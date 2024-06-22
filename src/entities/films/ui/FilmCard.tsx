c6fd4a7bd88862490c75dd3efd49176306686be7
BohdanTaran <bohdantaran.work@gmail.com>
2024-06-22 16:59:10 +0200

    refactor filmCard

--- src/entities/films/ui/FilmCard.tsx
+++ src/entities/films/ui/FilmCard.tsx
@@ -1,17 +1,27 @@
+import { AdultWarning } from '../../../shared/ui/AdultWarning';
+import { IGenre } from '../../../shared/ui/Genres/model/types';
+import { Genres } from '../../../shared/ui/Genres/ui/Genres';
 import { IFilm } from '../model/types';
 import styles from './FilmCard.module.css';
 
 interface Props {
   film: IFilm;
+  genres: IGenre[];
 }
 
 const BASE_IMAGE_URL = import.meta.env.VITE_BASE_IMAGE_URL;
 
-export const FilmCard = ({ film }: Props) => {
+export const FilmCard = ({ film, genres }: Props) => {
   return (
     <div className={styles.poster}>
       <img className={styles.img} src={`${BASE_IMAGE_URL}${film.poster_path}`} alt={film.title} />
-      <span className={styles.title}>{film.original_title}</span>
+      <div className={styles.filmCard_description}>
+        <div className={styles.sub_title}>
+          {film.adult && <AdultWarning />}
+          <Genres genres={genres} film={film} />
+        </div>
+        <span className={styles.title}>{film.original_title}</span>
+      </div>
     </div>
   );
 };
