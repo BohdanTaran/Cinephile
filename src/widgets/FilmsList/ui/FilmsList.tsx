98c269bf9104b50f61f7c5212cb6f2d1b0f8f079
BohdanTaran <bohdantaran.work@gmail.com>
2024-07-08 19:00:09 +0200

    add context for genres

--- src/widgets/FilmsList/ui/FilmsList.tsx
+++ src/widgets/FilmsList/ui/FilmsList.tsx
@@ -1,17 +1,15 @@
 import { FilmCard } from '../../../entities/films';
 import { IFilm } from '../../../entities/films/model/types';
-import { IGenre } from '../../../shared/ui/Genres/model/types';
 
 interface Props {
   films?: IFilm[];
-  genres: IGenre[];
 }
 
-export const FilmsList = ({ films, genres }: Props) => {
+export const FilmsList = ({ films }: Props) => {
   return (
     <>
       {films?.map((film) => {
-        return <FilmCard key={film.id} genres={genres} film={film} />;
+        return <FilmCard key={film.id} film={film} />;
       })}
     </>
   );
